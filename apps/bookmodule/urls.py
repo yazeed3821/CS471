db683f35298ba7a20d3da8b41d07c373c15d656e
yazeed <yazeed3821@gmail.com>
2026-04-03 13:31:58 +0300

    Lab5 work, 4 web pages

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('list_books/', views.list_books, name='books.list_books'),
     path('one_book/', views.one_book, name='books.one_book'),
     path('aboutus/', views.aboutus, name='books.aboutus'),
+    path('html5/links/', views.links),
+    path('html5/text/formatting/', views.formatting),
+    path('html5/listing/', views.listing),
+    path('html5/tables/', views.tables)
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -11,3 +11,15 @@ def one_book(request):
 
 def aboutus(request):
     return render(request, "bookmodule/aboutus.html")
+
+def links(request):
+    return render(request, "bookmodule/links.html")
+
+def formatting(request):
+    return render(request, "bookmodule/formatting.html")
+
+def listing(request):
+    return render(request, "bookmodule/listing.html")
+
+def tables(request):
+    return render(request, "bookmodule/tables.html")
